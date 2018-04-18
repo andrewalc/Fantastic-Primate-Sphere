812cd5f772f4d405599894f004d2251130f5aea3
Unknown <thelxie27@gmail.com>
2018-04-18 00:58:34 -0400

    Menu Screen
    Play button, goes to LevelOne

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour {
+
+	public Object nextScene;
+	public Button button;
+
+	// Use this for initialization
+	void Start () {
+		Button b = button.GetComponent<Button>();
+		b.onClick.AddListener(NextScene);
+	}
+	
+	void NextScene ()
+	{
+		SceneManager.LoadScene(nextScene.name);
+	}
+}
